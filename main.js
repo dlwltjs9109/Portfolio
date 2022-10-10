57d20a7cf2007c0b65031959ce517dcb7694e7dc
dlwltjs9109 <dlwltjs91098@naver.com>
2022-10-10 10:56:16 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Make Navbar transparent when it is on the top
+// 1. Make Navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -10,4 +10,17 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// 2. Handle scrolling  when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+    const link = event.target.dataset.link;
+    if(link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 });
