0e1448f83a567e4b68fb653f0a694b4dfe23d942
dlwltjs9109 <dlwltjs91098@naver.com>
2022-10-11 01:40:05 +0900

    JS : Navbar menu toggle + Project Ui tweak

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 // 2. Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 
@@ -20,17 +21,28 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null) {
         return;
     }
+
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
-// 3. Handle click on "contact me" button on home
+
+// 3. Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+
+// 4. Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
-// 4. Make home slowly fade to transparent as the window scrolls down
+
+// 5. Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
@@ -38,7 +50,8 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// 5. Show "arrow up" button when scrolling down
+
+// 6. Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 
 document.addEventListener('scroll', () => {
@@ -49,12 +62,13 @@ document.addEventListener('scroll', () => {
     }
 });
 
-// 5-1. Handle click on the "arrow up" button
+// 6-1. Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
-// 6. Projects
+
+// 7. Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
