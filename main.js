492c88f124c1c59bad40decac48f00e1034fd4cd
dlwltjs9109 <dlwltjs91098@naver.com>
2022-11-04 20:21:37 +0900

    js : add

--- main.js
+++ main.js
@@ -69,36 +69,6 @@ arrowUp.addEventListener('click', () => {
 
 
 // 7. Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-
-workBtnContainer.addEventListener('click', (event) => {
-    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
-    if(filter == null) {
-        return;
-    }
-
-    // Remove selection from the previous item select the new one
-    const active = document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
-    const target = event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode
-    target.classList.add('selected');
-    
-    projectContainer.classList.add('anim-out');
-    setTimeout(() => {
-        projects.forEach((project) => {
-            if(filter === '*' || filter === project.dataset.type) {
-                project.classList.remove('invisible');
-            } else {
-                project.classList.add('invisible');
-            }
-        });
-        projectContainer.classList.remove('anim-out');
-    }, 300);
-});
-
-
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
